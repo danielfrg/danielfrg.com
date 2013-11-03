f36cffc1d7abe81606b89df403eba7da39af2525
Daniel Rodriguez <drodriguez@oculus360.us>
2013-11-02 22:15:23 -0500

    Match settings on new version of pelican (3.3)

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*- #
 
-SITEURL = '/'
-# SITEURL = 'http://danielfrg.github.io'
+# SITEURL = ''
+SITEURL = 'http://danielfrg.github.io'
 AUTHOR = 'Daniel Rodriguez'
 SITENAME = 'Daniel Rodriguez'
 TIMEZONE = 'UTC'
@@ -26,10 +26,7 @@ PLUGINS = ['sitemap', 'ipythonnb']
 ARTICLE_URL = 'blog/{date:%Y}/{date:%m}/{date:%d}/{slug}/'
 ARTICLE_SAVE_AS = 'blog/{date:%Y}/{date:%m}/{date:%d}/{slug}/index.html'
 
-FILES_TO_COPY = (('robots.txt', 'robots.txt'),
-                 ('404.html', '404.html'),
-                 ('favicon.ico', 'favicon.ico'),
-                 ('google_verification.html', 'googleaa9959ae757fe3e1.html'),)
+STATIC_PATHS = ['content/static']
 
 # Theme settings
 HOMEPAGE_INTRO = "This blog is written by Daniel Rodriguez. He writes about coding, python, data science and his projects"
