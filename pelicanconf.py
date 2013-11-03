87a4d678468ddab35ff4e4cc839aaa48e0e6cb7a
Daniel Rodriguez <drodriguez@oculus360.us>
2013-11-02 23:42:27 -0500

    Fix settings

--- pelicanconf.py
+++ pelicanconf.py
@@ -9,24 +9,24 @@ DEFAULT_LANG = 'en'
 MARKUP = ('md', 'ipynb')
 DEFAULT_DATE_FORMAT = '%d.%m.%Y'
 
-PATH = './'
-PAGE_DIR = './pages'
-ARTICLE_DIR = './blog'
+PAGE_DIR = 'pages'
+ARTICLE_DIR = 'articles'
 
-THEME = "./themes/middle/"
+THEME = "themes/middle/"
 STATIC_PATHS = ["images"]
 
 SITEMAP = {
     'format': 'xml'
 }
 
-PLUGIN_PATH = './plugins'
+PLUGIN_PATH = 'plugins'
 PLUGINS = ['sitemap', 'ipythonnb']
 
 ARTICLE_URL = 'blog/{date:%Y}/{date:%m}/{date:%d}/{slug}/'
 ARTICLE_SAVE_AS = 'blog/{date:%Y}/{date:%m}/{date:%d}/{slug}/index.html'
 
-STATIC_PATHS = ['content/static']
+# paths inside `content`
+STATIC_PATHS = ['images', 'favicon.ico', '404.html', 'robots.html']
 
 # Theme settings
 HOMEPAGE_INTRO = "This blog is written by Daniel Rodriguez. He writes about coding, python, data science and his projects"
