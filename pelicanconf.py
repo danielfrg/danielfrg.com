f8eec835a0b5df10aa289be0c17f16d9f6290a6b
Daniel Rodriguez <drodriguez@oculus360.us>
2013-11-25 22:21:30 -0600

    new opensource fonts :)

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*- #
 
 SITEURL = ''
-SITEURL = 'http://danielfrg.github.io'
+# SITEURL = 'http://danielfrg.github.io'
 AUTHOR = 'Daniel Rodriguez'
 SITENAME = 'Daniel Rodriguez'
 TIMEZONE = 'UTC'
 DEFAULT_LANG = 'en'
 MARKUP = ('md', 'ipynb')
-DEFAULT_DATE_FORMAT = '%d.%m.%Y'
+DEFAULT_DATE_FORMAT = '%B %d, %Y'
 
 PAGE_DIR = 'pages'
 ARTICLE_DIR = 'articles'
@@ -25,16 +25,19 @@ PLUGINS = ['sitemap', 'ipythonnb']
 ARTICLE_URL = 'blog/{date:%Y}/{date:%m}/{date:%d}/{slug}/'
 ARTICLE_SAVE_AS = 'blog/{date:%Y}/{date:%m}/{date:%d}/{slug}/index.html'
 
-# paths inside `content`
+# Paths are relative to `content`
 STATIC_PATHS = ['images', 'favicon.ico', '404.html', 'robots.html']
 
 # Theme settings
-HOMEPAGE_INTRO = "This blog is written by Daniel Rodriguez. He writes about coding, python, data science and his projects"
+HOMEPAGE_INTRO = "Here is where Daniel Rodriguez writes about coding, python, data science and his projects"
+HOMEPAGE_DATE_FORMAT = '%d.%m.%Y'
 
 TWITTER_USERNAME = "danielfrg"
 GITHUB_USERNAME = "danielfrg"
-EMAIL_ADDRESS = "df.rodriguez143@gmail.com"
+LINKEDIN_PROFILE = "http://www.linkedin.com/profile/view?id=40926067"
 SHOW_FEED = True
+EMAIL_ADDRESS = "df.rodriguez143@gmail.com"
 
+METADATA_DATE_FORMAT = '%B %d, %Y'
 GOOGLE_ANALYTICS = 'UA-35523657-2'
 DISQUS_USERNAME = 'danielfrg'
