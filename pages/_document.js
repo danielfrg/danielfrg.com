b708d5c017cfc6366021c10dad898abb6a8fdad5
Daniel Rodriguez <daniel@danielfrg.com>
2022-01-13 12:35:29 -0600

    Update

--- pages/_document.js
+++ pages/_document.js
@@ -42,6 +42,11 @@ class MyDocument extends Document {
             `,
                         }}
                     />
+                    <script
+                        async
+                        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3750136993687031"
+                        crossOrigin="anonymous"
+                    ></script>
                 </Head>
 
                 <body>
