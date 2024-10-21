a38d03e7b2d3ed79358a785e479b7b007dc8fb60
Daniel Rodriguez <daniel@danielfrg.com>
2024-10-21 14:24:01 -0500

    update

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,7 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,ts,tsx,vue}"],
   darkMode: "class", // false or 'media' or 'class'
   theme: {
     extend: {
